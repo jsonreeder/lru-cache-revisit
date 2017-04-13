ee44edb54babd11eb2fdc3bb81b28c67af171313
Jason Reeder <info@jasonreeder.com>
2017-04-12 21:52:57 -0700

    Add hash set

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -9,18 +9,24 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count == num_buckets
+    @count += 1
+    self[key] << key
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    @count -= 1
+    self[key].delete(key)
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+  def [](key)
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +34,11 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    old_store.each do |bucket|
+      bucket.each { |el| insert(el) }
+    end
   end
 end
